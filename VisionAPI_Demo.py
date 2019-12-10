290d8f755c2027795f361b6bef3764782d720c15
teodorkasap <erol.erten@gmail.com>
2019-12-10 15:51:09 +0300

    working ocr detection script

--- VisionAPI_Demo.py
+++ VisionAPI_Demo.py
@@ -1,9 +1,41 @@
 import os, io
 from google.cloud import vision
 from google.cloud.vision import types
+import pandas as pd
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'test-for-receipt-OCR-e3f80220c980.json'
 
 client = vision.ImageAnnotatorClient()
 
 print(dir(client))
+
+file_name = 'IMG_20191210_151532.jpg'
+image_path = f'.\VisionAPI\Images\{file_name}'
+
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+# construct an iamge instance
+image = vision.types.Image(content=content)
+
+"""
+# or we can pass the image url
+image = vision.types.Image()
+image.source.image_uri = 'https://edu.pngfacts.com/uploads/1/1/3/2/11320972/grade-10-english_orig.png'
+"""
+
+# annotate Image Response
+response = client.text_detection(image=image)  # returns TextAnnotation
+df = pd.DataFrame(columns=['locale', 'description'])
+
+texts = response.text_annotations
+for text in texts:
+    df = df.append(
+        dict(
+            locale=text.locale,
+            description=text.description
+        ),
+        ignore_index=True
+    )
+
+print(df['description'][0])
